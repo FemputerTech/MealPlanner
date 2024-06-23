d3afcd1722cf84b9b1eb876490b0aea128168c47
FemputerTech <mleicht92@gmail.com>
2024-06-23 13:01:38 -0700

    added styling to the searchbar

--- static/index.js
+++ static/index.js
@@ -1,8 +1,23 @@
-var currentPath = window.location.pathname;
-
 // Find the <a> element with matching href and add 'active' class
+var currentPath = window.location.pathname;
 document.querySelectorAll('.navbar-nav .nav-link').forEach(function(link) {
     if (link.getAttribute('href') === currentPath) {
         link.classList.add('active');
     }
 });
+
+function activateSearchbar() {
+    console.log("activating search bar")
+    document.querySelector('.searchbar').classList.add('active');
+}
+
+function deactivateSearchbar() {
+    console.log("deactivating search bar")
+    document.querySelector('.searchbar').classList.remove('active');
+}
+
+function clearInput() {
+    var input = document.querySelector('.searchbar-input');
+    input.value = '';
+    input.focus(); // Keep focus on the input after clearing
+}
