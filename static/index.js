585ed67cc819380ae315d3c7b5166254b27cd499
FemputerTech <mleicht92@gmail.com>
2024-06-23 17:44:53 -0700

    created a grid layout similar to pinterest. Needs more tweaking though

--- static/index.js
+++ static/index.js
@@ -21,3 +21,18 @@ function clearInput() {
     input.value = '';
     input.focus(); // Keep focus on the input after clearing
 }
+
+/* Cards */
+const cards = document.querySelectorAll(".card-item");
+const cardClasses = ['card-small', 'card-regular', 'card-large'];
+
+// Function to pick a random class from cardClasses array
+function getRandomClass() {
+    return cardClasses[Math.floor(Math.random() * cardClasses.length)];
+}
+
+// Apply random classes to each card-item
+cards.forEach(card => {
+    const randomClass = getRandomClass();
+    card.classList.add(randomClass);
+});
