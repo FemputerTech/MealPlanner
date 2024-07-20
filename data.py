9aecd73c4669fc96f5131406338aad5a491bf0c6
FemputerTech <mleicht92@gmail.com>
2024-07-20 14:29:05 -0700

    added data for the popular items

---
+++ data.py
@@ -0,0 +1,41 @@
+popular_top = [
+    {
+        "title": "breakfast",
+        "image": "/static/images/breakfast.png"
+    },
+    {
+        "title": "lunch",
+        "image": "/static/images/lunch.png"
+    },
+    {
+        "title": "japanese",
+        "image": "/static/images/japanese.png"
+    },
+    {
+        "title": "vegan",
+        "image": "/static/images/vegan.png"
+    },
+    {
+        "title": "gluten-free",
+        "image": "/static/images/glutenfree.png"
+    },
+]
+
+popular_bottom = [
+    {
+        "title": "mexican",
+        "image": "/static/images/mexican.png"
+    },
+    {
+        "title": "dinner",
+        "image": "/static/images/dinner.png"
+    },
+    {
+        "title": "salad",
+        "image": "/static/images/salad.png"
+    },
+    {
+        "title": "italian",
+        "image": "/static/images/italian.png"
+    },
+]
